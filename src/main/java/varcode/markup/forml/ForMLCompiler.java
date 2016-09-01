eb9de920a9d06cc988becfda1e340b4010e2e723
edefazio <eric@typefra.me>
2016-09-01 13:18:49 -0400

    Add files via upload

---
+++ src/main/java/varcode/markup/forml/ForMLCompiler.java
@@ -0,0 +1,339 @@
+package varcode.markup.forml;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import varcode.context.VarContext;
+import varcode.form.BetweenTokens;
+import varcode.form.Form;
+import varcode.form.FormTemplate;
+import varcode.form.SeriesFormatter;
+import varcode.form.VarForm;
+import varcode.markup.MarkupException;
+
+/**
+ * Parses/Compiles the Text/ Markup of a {@code Form}
+ * and returns the appropriate {@code Form} implementation
+ * <UL>
+ *   <LI>{@code VarForm} if the Markup contains {@code MarkAction}s
+ *   <LI>{@code StaticForm} if the Markup does not contain {@code MarkAction}s 
+ * </UL>   
+ * @author M. Eric DeFazio eric@varcode.io
+ */
+public class ForMLCompiler
+{    
+	public static final String N = System.lineSeparator();
+	
+	/** The standard Context used for Parsing */
+	private static final VarContext PARSE_CONTEXT = VarContext.of( );
+	
+    /** The Default Instance */
+    public static final ForMLCompiler INSTANCE = 
+        new ForMLCompiler( 
+            PARSE_CONTEXT, 
+            BetweenTokens.BaseBetweenTokens.INSTANCE );
+    
+    private final BetweenTokens betweenTokens;
+    
+    /** parseContext contains the components for parsing */
+    private final VarContext parseContext;
+    
+    public ForMLCompiler(
+        VarContext parseContext,
+        BetweenTokens betweenTokens )
+    {
+        this.parseContext = parseContext;
+        this.betweenTokens = betweenTokens;        
+    }
+    
+    public Form compile( String forMLDoc )
+    {
+        return fromString( parseContext, -1, null, forMLDoc );
+    }
+    
+    public Form compile( String name, String forMLDoc )
+    {
+        return fromString( parseContext, -1, name, forMLDoc );
+    }
+    
+    public Form fromString( int lineNumber, String name, String forMLDoc )
+    {
+        return fromString( parseContext, lineNumber, name, forMLDoc );
+    }
+   
+    private static final BufferedReader readerFromString( String forMLDoc )
+    {
+        ByteArrayInputStream bais = 
+            new ByteArrayInputStream( 
+                forMLDoc.getBytes( StandardCharsets.UTF_8 ) );
+        
+         return new BufferedReader( 
+            new InputStreamReader( bais ) );
+    }
+    
+   
+    // {{ }} this "means" form and tail series test
+    // {_ _} this "means" form and NO tail Series test
+    public Form fromString( 
+        VarContext context, int lineNumber, String name, String text )
+    {   
+        // does the form text "end" with a between token? 
+        // if so, I need to chop this off BEFORE parsing the Form
+        String formNoTail = chopTailOf( text );
+    
+        FormTemplate formMarkup = 
+            compileTemplate(  
+                context, 
+                readerFromString( formNoTail ) );
+        
+        if( formMarkup.getMarks().length == 0 )
+        {   //there are NO marks (its just static text) 
+            return new Form.StaticForm( text );
+        }
+        //get the tail
+        String theTail = text.substring( formNoTail.length() );
+        
+        if( theTail.trim().length() == 0 )
+        {   //each Form instance doesnt have a tail
+            //a series of forms can be placed inline
+            VarForm vf = new VarForm(
+                formMarkup,
+                new SeriesFormatter.AfterEach( theTail ) );
+            return vf;
+        }
+        //I have a "Between" tail...
+        //let me get at the Tail that I Chopped off
+        String tailText = text.substring( 
+            formNoTail.length(), text.length() );
+        
+        VarForm vf = new VarForm( 
+            formMarkup, 
+            new SeriesFormatter.BetweenTwo( tailText ) );
+        return vf;        
+    }
+    
+    /**
+     * I need to return the Tail
+     * AND the Tail
+     * @param text
+     * @return
+     */
+    public String chopTailOf( String text )
+    {
+        String betweenToken = betweenTokens.endsWithToken( text );
+        //String betweenTail = null;
+        
+        if( betweenToken != null )
+        {   // The Text ENDS with a "Between" Token (like a ',', '&', etc.)
+            // which signifies when we have a series of Forms, they need
+            // to have this token to separate Tokens in a Series:
+            //<PRE>
+            // if the form is : "{+var}, "
+            //                         ^^
+            //</PRE>
+            // ...we recognize the "tail"
+            // and remove it, since when we put a series of forms together
+            // we want this:<PRE>
+            // "A, B, C"
+            //   ^^ ^^
+            //        
+            // NOT this (with a dangling separator):<PRE>
+            // "A, B, C, "</PRE>
+            //            
+            //found a between token, remove it from the "form" proper
+            String betweenTail 
+                = text.substring( text.lastIndexOf( betweenToken ) );            
+            
+            return text.substring( 0, text.length() - betweenTail.length() );
+        }
+        //The Form DOESNT end with a "BEtween Token", but lets chop off any trailing
+        // Whitespace ( spaces, tabs, lineFeeds )
+        //trim the tail
+        int lastIndex = text.length() -1;
+        while( lastIndex > 1 
+            && Character.isWhitespace( text.charAt( lastIndex ) ) )
+        {
+            lastIndex --;
+        }
+        text = text.substring( 0, lastIndex + 1 );
+        return text;              
+    }
+    
+    public static FormTemplate compileTemplate( String forML )
+    {
+    	return compileTemplate( VarContext.of( ), readerFromString( forML ) );
+    }
+    
+    public static FormTemplate compileTemplate( 
+        VarContext varContext,            
+        BufferedReader theReader )
+    {
+        ForMLParseState parseState = new ForMLParseState( varContext );
+        try
+        {
+            String line = "";
+            int lineNumber = 1; //initialize the line Number
+             
+            boolean firstLine = true;
+             
+            while( ( line = theReader.readLine() ) != null ) 
+            {
+                if( !firstLine )   
+                {   //"prepend" the new line before processing the line 
+                    if( parseState.isMarkOpen() ) 
+                    {   //previous line ended and a Mark wasn't closed
+                        parseState.addToMark( N );
+                    }
+                    else
+                    {   //move static text to next line
+                        parseState.addText( N ); 
+                    }
+                }
+                else 
+                {
+                    firstLine = false;
+                }               
+                if( parseState.isMarkOpen() ) 
+                {   //if previous MARKS aren't closed  
+                    lineOpenMark( 
+                        line,   
+                        lineNumber, 
+                        parseState );
+                }
+                else
+                {
+                    line( 
+                        line, 
+                        lineNumber, 
+                        parseState );                       
+                }           
+                lineNumber++;
+            }
+            //REACHED THE END OF Markup
+            if( parseState.isMarkOpen() )
+            {   //UNCLOSED MARK
+                throw new MarkupException( 
+                    "Unclosed Mark with text " + N 
+                    + parseState.getMarkContents() );
+            }
+                return parseState.compile();
+            }
+            catch( IOException ioe )            
+            {
+                throw new MarkupException( 
+                    "Problem reading from Reader ", ioe );
+            }
+            finally
+            {
+                try
+                {
+                    theReader.close();
+                }
+                catch( IOException e )
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        /**
+         * Process the next line of text from the code varcode source, 
+         * knowing that a mark (from some previous line) has not been closed.<BR>
+         *  
+         * (a Mark spans multiple lines)
+         * 
+         * @return an Empty StringBuilder if we have closed the marks 
+         *         -or- a StingBuilder containing internal Mark data  from the line 
+         */
+        private static void lineOpenMark( 
+            String sourceLine, 
+            int lineNumber, 
+            ForMLParseState builder )
+        {   //check if the open Mark is closed this line
+            String closeTag = builder.getCloseTagForOpenMark();
+            
+            int indexOfCloseMark = 
+                sourceLine.indexOf( closeTag );
+            
+            if( indexOfCloseMark >= 0 )
+            {
+                builder.completeMark( 
+                    sourceLine.substring( 
+                        0, 
+                        indexOfCloseMark + closeTag.length() ),                 
+                    lineNumber );
+                
+                //process what is left of the line (after the close mark)
+                line( 
+                    sourceLine.substring( indexOfCloseMark + closeTag.length() ), 
+                    lineNumber, 
+                    builder );
+                return;
+            }
+            builder.addToMark( sourceLine );
+        }       
+        
+   /**
+    * Process the data within the {@code sourceLine} at {@code lineNumber}
+    * (update the {@code builder} with any tags, internal test or document
+    * text  
+    * 
+    * @param sourceLine the source line to parse
+    * @param lineNumber 
+    * @param builder updates the {@code VarCode} being built
+    */
+    private static void line( 
+        String sourceLine, 
+        int lineNumber, 
+        ForMLParseState builder )
+    {   //this is the CLOSE Mark for EITHER ALONE or REPLACE Marks          
+        
+        String firstOpenTag = ForMLParser.INSTANCE.getFirstOpenTag( sourceLine );
+         
+        if( firstOpenTag != null )
+        {   //Opened a mark this line
+            int indexOfOpenMark = sourceLine.indexOf( firstOpenTag );
+            //add everything before the open tag
+            builder.addText( sourceLine.substring( 0, indexOfOpenMark ) );
+               
+            String matchingCloseTag = 
+            	ForMLParser.INSTANCE.closeTagFor( firstOpenTag );
+                
+            //find a close tag AFTER the OPEN Tag
+            int closeTagIndex = sourceLine.indexOf( 
+                matchingCloseTag, indexOfOpenMark + firstOpenTag.length() );
+                
+            if( closeTagIndex >= 0 )
+            {   //the Mark is closed this line
+                builder.completeMark( 
+                    sourceLine.substring( 
+                        indexOfOpenMark, 
+                        closeTagIndex + matchingCloseTag.length() ), 
+                    lineNumber );
+                    
+                //process the rest of the line (AFTER the CLOSE TAG of MARK)
+                line( 
+                    sourceLine.substring( 
+                        closeTagIndex + matchingCloseTag.length(), 
+                        sourceLine.length() ),
+                    lineNumber,                 
+                    builder );
+                return;
+            }
+            else
+            {   //the mark is not closed this line
+                builder.startMark( 
+                    sourceLine.substring( indexOfOpenMark ), 
+                    firstOpenTag );
+                return;
+            }
+        }
+        else
+        {   //NO Tags/ Marks this line (just text)
+            builder.addText( sourceLine );
+        }           
+    }       
+}
---
+++ src/main/java/varcode/markup/forml/ForMLParseState.java
@@ -0,0 +1,194 @@
+package varcode.markup.forml;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
+import varcode.context.VarContext;
+import varcode.doc.FillInTheBlanks;
+import varcode.form.FormTemplate;
+import varcode.markup.MarkupException;
+import varcode.markup.ParseState;
+import varcode.markup.mark.Mark;
+import varcode.markup.mark.Mark.BlankFiller;
+
+/**
+ * Mutable Intermediate Representation (IR) Parse State for producing a 
+ * {@code Form} while source is being read in and parsed
+ * 
+ * Contains {@code MarkActions}s assigned to FillBlanks and static text.
+ * 
+ * Internally acts like a "State Machine" that follows: 
+ * 
+ * <OL>
+ *   <LI>seek next open tag within the text (at {@code tagOpenIndex})
+ *   <LI>add all static text between {@code charCursor} and 
+ *   {@code tagOpenIndex} to the state
+ *   <LI>seek the matching "close" tag {@code tagCloseIndex} within the 
+ *   text after the {@code tagOpenIndex}.
+ *   <LI>using the {@code MarkParser} parse the Mark text between {@code tagOpenIndex}
+ *   and {@code tagCloseIndex} and add the parsed {@code Mark} to the {@code CompileState} 
+ *   (signifying the blank if appropriate)
+ *   <LI>set {@code charCursor} to after {@code tagCloseIndex}
+ *   <LI>goto step (1) until the end of document is reached  
+ * </OL>
+ * 
+ * The following methods mutate the contents of the State
+ * <UL>
+ * <LI>startMark
+ * <LI>appendToMark
+ * <LI>completeMark
+ * <LI>appendText
+ * </UL>
+ * 
+ * @author M. Eric DeFazio eric@varcode.io
+ */
+public class ForMLParseState 
+    implements ParseState
+{
+    public static final String N = System.lineSeparator();
+    
+    /** All static text and a BitSet indicating {@code MarkAction}s locations */
+    public final FillInTheBlanks.Builder fillBuilder;
+    
+    /** stores the character indexes of ALL {@code Mark}s within the form markup */
+    public final BitSet allMarkIndexes;
+    
+    /** All marks extracted from the form markup */  
+    public final List<Mark> allMarks;
+
+    /** If the current mark is open, the matching close tag to complete the mark
+     * (otherwise null) */ 
+    private String closeTagForCurrentOpenMark;
+    
+    /** Buffer for text inside a Mark that spans lines */
+    private final StringBuilder markBuffer;
+    
+    /** contains any modules for parsing (effectively immutable) */
+    private final VarContext parseContext;
+
+    /** *MUTABLE* current character cursor index within the markup */
+    public int cursorIndex;
+    
+    public ForMLParseState()
+    {
+    	this( VarContext.of() );
+    }
+    
+    public ForMLParseState( VarContext parseContext )
+    {
+        this.fillBuilder = new FillInTheBlanks.Builder();
+        this.allMarkIndexes = new BitSet();
+        this.allMarks = new ArrayList<Mark>();
+        this.parseContext = parseContext;
+        
+        this.markBuffer = new StringBuilder();
+        this.cursorIndex = 0;
+    }
+    
+    public boolean isMarkOpen()
+    {
+        return markBuffer.length() > 0;
+    }
+    
+    /** add "static" text to the document */ 
+    public void addText( String staticText )
+    {
+        this.fillBuilder.text( staticText );
+        this.cursorIndex += staticText.length();
+    }
+    
+    /** Append text to the exiting open mark*/
+    public void startMark( String markText, String openTag )
+    {
+        this.closeTagForCurrentOpenMark = ForMLParser.INSTANCE.closeTagFor( openTag );
+        this.markBuffer.append( markText );
+    }
+    
+    /** adds text to a Mark Buffer */
+    public void addToMark( String markText )
+    {
+        this.markBuffer.append( markText );
+    }
+    
+    /**Reserves a Fillable blank at the current char position in the Form */
+    public void reserveBlank()
+    {
+    	this.fillBuilder.blank(); 
+    }
+    
+    /** 
+     * Add content and the closing tag to the current (buffered) mark, 
+     * then create/add the {@code MarkAction} and return the State
+     * @param contentWithCloseTag
+     * @param lineNumber
+     */
+    public void completeMark( String contentWithCloseTag, int lineNumber )
+    {
+        if( isMarkOpen() && 
+            !contentWithCloseTag.endsWith( closeTagForCurrentOpenMark ) )
+        {
+            throw new MarkupException(
+                "could not complete a Mark : " + N
+              + markBuffer.toString() + N + " with : " + N 
+              + contentWithCloseTag + N  
+              + "... expected content ending with closing Mark \""
+              + closeTagForCurrentOpenMark + "\"" );
+        }
+        markBuffer.append( contentWithCloseTag );
+        
+        
+        Mark ma = ForMLParser.INSTANCE.parseMark( 
+        	parseContext,	
+            markBuffer.toString(), 
+            lineNumber );
+        allMarks.add( ma );
+        
+        allMarkIndexes.set( cursorIndex );
+        
+        this.cursorIndex ++;
+        
+        if( ma instanceof BlankFiller )
+        {
+        	reserveBlank();
+        }           
+        resetMark();
+    }
+    
+    public String getCloseTagForOpenMark()
+    {
+        return this.closeTagForCurrentOpenMark;
+    }
+    
+    public String getMarkContents()
+    {
+        return markBuffer.toString();
+    }
+    
+    private void resetMark()
+    {
+        markBuffer.delete( 0, markBuffer.length() );
+        closeTagForCurrentOpenMark = null;
+    }
+    
+    /** 
+     * Convert the mutable Intermediate Representation (parsed by the compiler) 
+     * into an immutable {@code VarCode}.
+     * 
+     * @return the immutable {@VarCode}  
+     */
+    public FormTemplate compile() 
+    {
+        if( isMarkOpen() )
+        {   //UNCLOSED MARK
+            throw new MarkupException( 
+                "Unclosed Form Mark :" + N 
+              + getMarkContents() + N 
+              + "...expected close Tag \"" + this.closeTagForCurrentOpenMark + "\"" );
+        }    
+        return new FormTemplate( 
+            fillBuilder.compile(), 
+            allMarks.toArray( new Mark[ 0 ] ),
+            allMarkIndexes );
+    }
+}
