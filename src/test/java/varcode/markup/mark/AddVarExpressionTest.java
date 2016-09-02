145548b01d9ceffaff9062df10bb3a6dc5639b27
edefazio <edefazio@typefra.me>
2016-09-02 11:55:33 -0400

    spacing

--- src/test/java/varcode/markup/mark/AddVarExpressionTest.java
+++ src/test/java/varcode/markup/mark/AddVarExpressionTest.java
@@ -3,8 +3,6 @@ package varcode.markup.mark;
 import varcode.VarException;
 import varcode.context.VarContext;
 import varcode.context.VarBindException.NullVar;
-import varcode.eval.EvalException;
-import varcode.markup.mark.AddVarIsExpression;
 import junit.framework.TestCase;
 import varcode.context.VarBindException;
 
@@ -17,130 +15,130 @@ import varcode.context.VarBindException;
 ))*+}*/
 
 public class AddVarExpressionTest 
-	extends TestCase
+    extends TestCase
 {	
-	static final String name = "access";
-	static final String EXPRESSION = 
-		"['public','','protected','private'].indexOf( access ) >= 0 ";
-	static final int lineNumber = 0;
+    static final String name = "access";
+    static final String EXPRESSION = 
+	"['public','','protected','private'].indexOf( access ) >= 0 ";
+    static final int lineNumber = 0;
 	
-	public void testValidMark()
-	{
-		String mark =
-			"/*{+" + name + ":(( " + EXPRESSION + " ))"+ "+}*/";
+    public void testValidMark()
+    {
+        String mark =
+            "/*{+" + name + ":(( " + EXPRESSION + " ))"+ "+}*/";
 		
-		AddVarIsExpression ave = 
-			new AddVarIsExpression(
-			 	mark, 
-			    lineNumber,
-			    name, 
-			    false, //boolean isRequired,
-			    EXPRESSION,
-			    "" );		
-		assertEquals( "",ave.derive( VarContext.of() ) );
-		assertEquals( "public", ave.derive( VarContext.of( "access", "public" ) ) );
-		assertEquals( "private", ave.derive( VarContext.of( "access", "private" ) ) );
-		assertEquals( "protected", ave.derive( VarContext.of( "access", "protected" ) ) );
-		assertEquals( "", ave.derive( VarContext.of("access", "" ) ) );		
-	}
+	AddVarIsExpression ave = 
+	    new AddVarIsExpression(
+		mark, 
+		lineNumber,
+		name, 
+		false, //boolean isRequired,
+		EXPRESSION,
+		"" );		
+        
+	assertEquals( "",ave.derive( VarContext.of() ) );
+	assertEquals( "public", ave.derive( VarContext.of( "access", "public" ) ) );
+	assertEquals( "private", ave.derive( VarContext.of( "access", "private" ) ) );
+	assertEquals( "protected", ave.derive( VarContext.of( "access", "protected" ) ) );
+	assertEquals( "", ave.derive( VarContext.of("access", "" ) ) );		
+    }
 	
-	
-	 public void testRequired()
-	 {
-	        //NOTE: here I want the name with the first character uppercase
-		 	String mark =
-				"/*{+name*+}*/";
+    public void testRequired()
+    {
+        //NOTE: here I want the name with the first character uppercase
+        String mark =
+            "/*{+name*+}*/";
 				
-			AddVarIsExpression ave = 
-				new AddVarIsExpression(
-					 mark, 
-					 lineNumber,
-					 "name", 
-					 true, //boolean isRequired,
-					 null,
-					 null );	
+	AddVarIsExpression ave = 
+	    new AddVarIsExpression(
+	        mark, 
+		lineNumber,
+		"name", 
+		true, //boolean isRequired,
+		null,
+		null );	
 	        
-	        assertTrue( ave.isRequired() );
-	        assertEquals( ave.getVarName(),  "name" );
-	        
-	        assertEquals( "eric", ave.derive( VarContext.of( "name", "eric" ) ) );
-	        
-	        //assertEquals( "Eric", i.derive( VarContext.of( "name", "Eric" ) ) );
-	        
-	        try
-	        {
-	            ave.derive( VarContext.of( ) );
-	            fail("Expected Exception for Missing Required Field ");
-	        }
-	        catch( VarException cme )
-	        {
-	            //expected
-	        }
-	    }
-	 
-	public void testRequiredButNullMark()
-	{
-		String mark =
-			"/*{+" + name + ":(( " + EXPRESSION + " ))"+ "*+}*/";
-		
-		AddVarIsExpression ave = 
-			new AddVarIsExpression(
-			 	mark, 
-			    lineNumber,
-			    name, 
-			    true, //boolean isRequired,
-			    EXPRESSION,
-			    null );		
-		try
-		{
-			ave.derive( VarContext.of() );
-			fail("Expected exception for required but null");
-		}
-		catch( NullVar rbn )
-		{
-			//expected
-		}		
-	}
+	assertTrue( ave.isRequired() );
+	assertEquals( ave.getVarName(),  "name" );
 	
-	public void testBadButDefaultMark()
+        assertEquals( "eric", ave.derive( VarContext.of( "name", "eric" ) ) );
+	        
+	//assertEquals( "Eric", i.derive( VarContext.of( "name", "Eric" ) ) );
+	        
+	try
 	{
-		String mark =                                    //default "public"
-			"/*{+" + name + ":(( " + EXPRESSION + " ))"+ "|public+}*/";
-		
-		AddVarIsExpression ave = 
-			new AddVarIsExpression(
-			 	mark, 
-			    lineNumber,
-			    name, 
-			    false, //boolean isRequired,
-			    EXPRESSION,
-			    "public" ); //default		
-		
-		assertEquals( "public", ave.derive( VarContext.of() ) );
-		assertEquals( "public", ave.derive( VarContext.of( "access", "Q@&*(^$" ) ) );
+            ave.derive( VarContext.of( ) );
+	    fail("Expected Exception for Missing Required Field ");
 	}
-
-	public void testBadMark()
+        catch( VarException cme )
 	{
-		String mark =
-			"/*{+" + name + ":(( " + EXPRESSION + " ))"+ "*+}*/";
+            //expected
+	}
+    }
+	 
+    public void testRequiredButNullMark()
+    {
+        String mark =
+	    "/*{+" + name + ":(( " + EXPRESSION + " ))"+ "*+}*/";
 		
-		AddVarIsExpression ave = 
-			new AddVarIsExpression(
-			 	mark, 
-			    lineNumber,
-			    name, 
-			    true, //boolean isRequired,
-			    EXPRESSION,
-			    null );		
-		try
-		{
-			ave.derive( VarContext.of( "access", "Q@&*(^$" ) );
-			fail("expected Exception for bad name");
-		}
-		catch( VarBindException ee )
-		{
-			ee.printStackTrace();
-		}
-	}	
+        AddVarIsExpression ave = 
+	    new AddVarIsExpression(
+                mark, 
+		lineNumber,
+		name, 
+		true, //boolean isRequired,
+		EXPRESSION,
+		null );		
+	try
+        {
+            ave.derive( VarContext.of() );
+            fail("Expected exception for required but null");
+        }
+        catch( NullVar rbn )
+        {
+            //expected
+        }		
+    }
+	
+    public void testBadButDefaultMark()
+    {
+        String mark =                                    //default "public"
+            "/*{+" + name + ":(( " + EXPRESSION + " ))"+ "|public+}*/";
+		
+        AddVarIsExpression ave = 
+            new AddVarIsExpression(
+		mark, 
+                lineNumber,
+		name,
+                false, //boolean isRequired,
+                EXPRESSION,
+		"public" ); //default		
+		
+	assertEquals( "public", ave.derive( VarContext.of() ) );
+	assertEquals( "public", ave.derive( VarContext.of( "access", "Q@&*(^$" ) ) );
+    }
+
+    public void testBadMark()
+    {
+        String mark =
+            "/*{+" + name + ":(( " + EXPRESSION + " ))"+ "*+}*/";
+		
+	AddVarIsExpression ave = 
+            new AddVarIsExpression(
+		mark, 
+                lineNumber,
+		name, 
+                true, //boolean isRequired,
+		EXPRESSION,
+		null );		
+	try
+	{
+            ave.derive( VarContext.of( "access", "Q@&*(^$" ) );
+            fail("expected Exception for bad name");
+	}
+	catch( VarBindException ee )
+	{
+            ee.printStackTrace();
+	}
+    }	
 }
