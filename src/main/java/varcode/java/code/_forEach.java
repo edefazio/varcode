5edc651af710e8736beb7a0d5c771db0a351cfbd
edefazio <edefazio@typefra.me>
2016-09-25 12:14:30 -0400

    modified parameter order to be more natural

--- src/main/java/varcode/java/code/_forEach.java
+++ src/main/java/varcode/java/code/_forEach.java
@@ -15,53 +15,168 @@
  */
 package varcode.java.code;
 
-import java.util.ArrayList;
-import java.util.List;
+import varcode.Template;
+import varcode.context.VarContext;
+import varcode.doc.Author;
+import varcode.doc.Directive;
 import varcode.doc.translate.JavaTranslate;
-import varcode.doc.translate.TranslateBuffer;
+import varcode.dom.Dom;
+import varcode.markup.bindml.BindML;
 
 /**
  *
  * @author M. Eric DeFazio eric@varcode.io
  */
 public class _forEach
+    extends Template.Base
 {
-    /** 
-     * OK its now apparent the Translator is being used 
-     * without the buffer part, so I need to separate that out
-     * in the future
-     */
-    static TranslateBuffer tb = new TranslateBuffer();    
-    
-    
-    /**
+        /**
+     * builds a forEach statement with body: <PRE>
+     * _forEach( int.class, "x", "array")
+     *     .body("System.out.printlnt(x);");
      * 
+     * for( int x : array )
+     * {
+     *     System.out.println(x);
+     * }
+     * </PRE>
      * @param collection the variable name of the collection or array
      * @param elementType the element type within the collection
      * @param elementName the name for each element
      * @return _forEach that contains a block
      */
+    public static _forEach of( 
+        Class elementType, String elementName, String collection)
+    {
+        return new _forEach( 
+            collection, 
+            JavaTranslate.INSTANCE.translate( elementType ),  
+            elementName );
+    }
+    
+    /**
+     * builds a forEach statement with body: <PRE>
+     * _forEach( "String", "name", "names")
+     *     .body("System.out.printlnt(name);");
+     * 
+     * for( String name : names )
+     * {
+     *     System.out.println(name);
+     * }
+     * </PRE>
+     * @param collection the variable name of the collection or array
+     * @param elementType the element type within the collection
+     * @param elementName the name for each element
+     * @return _forEach that contains a block
+     */
+    public static _forEach of( 
+        String elementType, String elementName, String collection)
+    {
+        return new _forEach( collection, elementType, elementName );
+    }
+    
+    public static Dom FOREACH = BindML.compile( 
+        "for( {+elementType*+} {+elementName*+} : {+collection*+} )" + N +    
+        "{" + N + 
+        "{{+?body:{+$>(body)+}" + N + 
+        "+}}" +         
+        "}"                        
+    );
+    
+    private String collection;
+    private String elementType;
+    private String elementName;
+    private _code body;
+    
+    /**
+     * 
+     * @param collection
+     * @param elementType
+     * @param elementName 
+     */
+    public _forEach( String collection, String elementType, String elementName )
+    {
+       this.collection = collection;
+       this.elementType = elementType;
+       this.elementName = elementName;
+       this.body = new _code();
+    }
+    
+    
+    @Override
+    public _forEach bindIn(VarContext context)
+    {
+        this.collection = Author.code( BindML.compile(this.collection), context );
+        this.elementType = Author.code( BindML.compile(this.elementType), context );
+        this.elementName = Author.code( BindML.compile(this.elementName), context );
+        this.body.bindIn( context );
+        return this;
+    }
+
+    @Override
+    public _forEach replace(String target, String replacement)
+    {
+        this.collection = this.collection.replace(target, replacement);
+        this.elementName = this.elementName.replace( target, replacement);
+        this.elementType = this.elementType.replace( target, replacement );
+        this.body.replace(target, replacement);
+        return this;
+    }
+
+    public VarContext getContext()
+    {
+        return VarContext.of(
+            "elementType", this.elementType, 
+            "elementName", this.elementName,
+            "collection", this.collection, 
+            "body", this.body );
+    }
+
+    public String getCollection()
+    {
+        return collection;
+    }
+
+    public String getElementType()
+    {
+        return elementType;
+    }
+
+    public String getElementName()
+    {
+        return elementName;
+    }
+    
+    public _code getBody()
+    {
+        return this.body;
+    }
+    
+    public _forEach body( Object... codeLines )
+    {
+        this.body.addTailCode( codeLines );
+        return this;
+    }
+    
+    @Override
+    public String author( Directive... directives )
+    {
+        return Author.code( FOREACH, getContext(), directives );
+    }
+    
+    @Override
+    public String toString()
+    {
+        return author( );
+    }
+
+    /*
     public static String _each( 
         String collection, Class elementType, String elementName )
     {
-        return "for( " + JavaTranslate.INSTANCE.translate( elementType ) + " " +  elementName +" : "+ collection +" )";
+        return "for( " + 
+        JavaTranslate.INSTANCE.translate( elementType ) + " " +  elementName 
+                +" : "+ collection +" )";
     }
-    
-    
-    public static void main(String[] args)
-    {
-        String[] strings = new String[]{ "a", "b" };
-        for( String a : strings )
-        {
-            System.out.println( a );
-        }        
-        System.out.println( _each( "strings", String.class, "s" ) );
-        System.out.println( _each( "list", int.class, "i" ) );
-        
-        List<Integer> list = new ArrayList<Integer>();
-        for( Integer u : list )
-        {
-            System.out.println( u );
-        }
-    }
+    */
 }
