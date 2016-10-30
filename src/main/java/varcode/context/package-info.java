7d5063a69cabe203b5fffd61c4a023be121cd891
edefazio <edefazio@typefra.me>
2016-10-30 09:36:38 -0400

    code cleanup

--- src/main/java/varcode/context/package-info.java
+++ src/main/java/varcode/context/package-info.java
@@ -1,5 +1,6 @@
 /**
- * binds data and functionality to names that are to be used
- * when authoring documents.
+ * stores contextual data and functionality by name to be used
+ * when authoring documents. 
+ * (The "variable" parts of documents are stored in the VarContext)
  */
 package varcode.context;
